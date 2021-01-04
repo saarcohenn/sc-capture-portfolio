477b540c8baa2825bb588fffdbb22cd4155e57ec
saar cohen <saar.cohenn@gmail.com>
2021-01-04 16:05:00 +0200

    [Added] - About Us section
    I have created a  'AboutSection' component, which is included in 'AboutUs' Page which is another component

---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+const AboutSection = () => {
+  return (
+    <div className="">
+      <div className="description">
+        <div className="title">
+          <div className="hide">
+            <h2>We work to make</h2>
+          </div>
+          <div className="hide">
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className="hide">
+            <h2>true.</h2>
+          </div>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas you have. we have
+          pros with amazing skills
+        </p>
+        <button>Contact Us</button>
+        <img src={home1} alt="guy with a camera" />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
