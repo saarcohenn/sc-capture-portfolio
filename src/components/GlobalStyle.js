354c23a789b011901d16bae88064d543e410629a
saar cohen <saar.cohenn@gmail.com>
2021-01-07 12:55:49 +0200

    [Added] - Styling Services Section
    Add styels.js to containe generic styled components. that will help us reuse the code and prevent code duplication;

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body {
     background: #1b1b1b;
+    font-family: 'Inter',sans-serif;
 }
 
 button {
@@ -21,6 +22,8 @@ button {
     background: transparent;
     color: white;
     transition: all 0.5s ease;
+    font-family: 'Inter',sans-serif;
+
 
     &:hover {
         background-color:#23d997;
@@ -47,7 +50,7 @@ a {
     font-size: 1rem;
 }
 p {
-    padding: 3rrem 0rem;
+    padding: 3rem 0rem;
     color: #ccc;
     font-size: 1.4rem;
     line-height: 150%normal;
