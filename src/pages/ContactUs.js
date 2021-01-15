1ccb1a1b3e86d9636363e8db7ce53bb76180b7c0
saar cohen <saar.cohenn@gmail.com>
2021-01-15 13:55:00 +0200

    [Added] - ContactUs page wascreated
    I have added some content to the page and some styles and animations.

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,19 +1,69 @@
 import React from "react";
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnimation } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <ContactStyled
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <Title>
+        <Hide>
+          <motion.h2 variants={titleAnimation}>Get in touch.</motion.h2>
+        </Hide>
+      </Title>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send Us A Message</h2>
+        </Social>
+      </Hide>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send Us A Message</h2>
+        </Social>
+      </Hide>
+      <Hide>
+        <Social variants={titleAnimation}>
+          <Circle />
+          <h2>Send Us A Message</h2>
+        </Social>
+      </Hide>
+    </ContactStyled>
   );
 };
+
+const ContactStyled = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+  background: white;
+`;
+const Title = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+const Social = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;
